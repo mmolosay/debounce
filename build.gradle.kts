d6456053b5a94e136652c7c62f5b6f6bcfe2f68a
mmolosay <mmolosays@gmail.com>
2023-04-07 14:19:28 +0200

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    val agpV = "7.3.1"
+    val kotlinV = "1.8.10"
+
+    id("com.android.application") version agpV apply false
+    id("com.android.library") version agpV apply false
+    id("org.jetbrains.kotlin.android") version kotlinV apply false
+}
