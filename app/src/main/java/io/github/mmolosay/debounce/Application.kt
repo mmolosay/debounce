461e3c431dbd1abd663c806a4f0ce118bb0d4af6
mmolosay <mmolosays@gmail.com>
2023-04-10 17:10:16 +0200

    Refine UI

--- app/src/main/java/io/github/mmolosay/debounce/Application.kt
+++ app/src/main/java/io/github/mmolosay/debounce/Application.kt
@@ -2,10 +2,12 @@ package io.github.mmolosay.debounce
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
@@ -21,10 +23,13 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.mmolosay.debounce.lib.debounced
+import java.text.SimpleDateFormat
 import java.util.Date
 import kotlin.time.Duration.Companion.seconds
 
@@ -55,17 +60,23 @@ private fun MainContent(
     modifier: Modifier = Modifier,
 ) {
     val events = remember { mutableStateListOf<ClickEvent>() }
+    val locale = LocalConfiguration.current.locales[0]
+    val dateFormatter = remember(locale) { SimpleDateFormat("HH:mm:ss:SSS", locale) }
     LazyColumn(
         modifier = modifier.fillMaxSize(),
         contentPadding = PaddingValues(all = 16.dp),
-        verticalArrangement = Arrangement.Top,
+        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.Top),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Button(
-            afterClick = { events.add(ClickEvent(date = Date(), wasInvoked = it)) },
-        )
+        Button { wasOnClickExecuted ->
+            events += ClickEvent(
+                time = dateFormatter.format(Date()),
+                wasExecuted = wasOnClickExecuted,
+                background = inferClickEventBackground(wasOnClickExecuted),
+            )
+        }
         ClickEvents(
-            items = events,
+            items = events.reversed(),
         )
     }
 }
@@ -78,7 +89,7 @@ private fun LazyListScope.Button(
         val onClick = remember {
             debounced(
                 timeout = 3.seconds,
-                onInvoke = { afterClick(it) },
+                onInvoke = { wasExecuted -> afterClick(wasExecuted) },
                 action = { onButtonClick(context) },
             )
         }
@@ -95,12 +106,22 @@ private fun LazyListScope.ClickEvents(
 ) {
     items(
         items = items,
-        key = { it.date.time },
+        key = { it.time },
     ) {
-        Column {
-            Text(text = "Date: ${it.date}")
-            Text(text = "Was invoked: ${it.wasInvoked}")
-        }
+        ClickEventItem(item = it)
+    }
+}
+
+@Composable
+private fun ClickEventItem(item: ClickEvent) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(item.background)
+            .padding(4.dp),
+    ) {
+        Text(text = "Invokation time: ${item.time}")
+        Text(text = "Was executed: ${item.wasExecuted}")
     }
 }
 
@@ -111,6 +132,11 @@ private fun onButtonClick(
 
 @Stable
 private data class ClickEvent(
-    val date: Date,
-    val wasInvoked: Boolean,
+    val time: String,
+    val wasExecuted: Boolean,
+    val background: Color,
 )
+
+private fun inferClickEventBackground(wasOnClickExecuted: Boolean): Color =
+    if (wasOnClickExecuted) Color.Green.copy(alpha = 0.2f)
+    else Color.Red.copy(alpha = 0.2f)
