4318cc5d30d9df9aef3c745d51e98e997c2c456a
mmolosay <mmolosays@gmail.com>
2023-04-10 22:41:27 +0200

    Add test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,16 @@ repositories {
     mavenCentral()
 }
 
+tasks.test {
+    useJUnitPlatform()
+}
+
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+dependencies {
+    testImplementation("io.kotest:kotest-assertions-core:5.5.5")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
