37171a6d5ae40d77a95fcfb9271b12a4875ecb2c
mmolosay <mmolosays@gmail.com>
2023-04-12 19:24:14 +0200

    Add publishing and signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,90 @@
-plugins {
-    kotlin("jvm") version "1.8.20"
-}
+// region Project
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
+group = "io.github.mmolosay"
+version = "1.0.0"
+
+// endregion
+
+// region Plugins & dependencies
 
 repositories {
     mavenCentral()
 }
 
-tasks.test {
-    useJUnitPlatform()
+plugins {
+    kotlin("jvm") version "1.8.20"
+    id("org.gradle.java-library")
+    id("org.gradle.maven-publish")
+    id("org.gradle.signing")
 }
 
 dependencies {
     testImplementation("io.kotest:kotest-assertions-core:5.5.5")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
+
+// endregion
+
+// region Publishing & signing
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "debounce"
+            from(components["java"])
+            pom {
+                name.set("debounce")
+                description.set("Debounce your lambdas.")
+                url.set("https://github.com/mmolosay/debounce")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("mmolosay")
+                        name.set("Misha Malasai")
+                        email.set("mmolosays@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/mmolosay/debounce.git")
+                    developerConnection.set("scm:git:ssh://github.com/mmolosay/debounce.git")
+                    url.set("https://github.com/mmolosay/debounce")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "MavenCentral"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = properties["ossrhUsername"] as String
+                password = properties["ossrhPassword"] as String
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+
+// endregion
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
