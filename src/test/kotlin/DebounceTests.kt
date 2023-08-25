94457804e96329259b9a70fe013f94ef51ce4e3f
mmolosay <mmolosays@gmail.com>
2023-08-25 21:29:54 +0200

    Add tests for 'debounce()' function

---
+++ src/test/kotlin/DebounceTests.kt
@@ -0,0 +1,61 @@
+import io.github.mmolosay.debounce.DebounceStateIdentityImpl
+import io.github.mmolosay.debounce.debounce
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+
+/*
+ * Copyright 2023 Mikhail Malasai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+internal class DebounceTests {
+
+    val state = DebounceStateIdentityImpl()
+
+    @Test
+    fun `debounce() executes action when called for the first time`() {
+        var wasActionExecuted = false
+
+        state.debounce { wasActionExecuted = true }
+
+        wasActionExecuted shouldBe true
+    }
+
+    @Test
+    fun `debounce() does not execute action when called for subsequent times`() {
+        var actionExecutedTimes = 0
+
+        state.debounce { /* first execution */ }
+        state.debounce { actionExecutedTimes++ }
+        state.debounce { actionExecutedTimes++ }
+
+        actionExecutedTimes shouldBe 0
+    }
+
+    @Test
+    fun `debounce() returns true if action was executed`() {
+        val result = state.debounce { /* executed */ }
+
+        result shouldBe true
+    }
+
+    @Test
+    fun `debounce() returns false if action was not executed`() {
+        val result = state.debounce { /* executed */ }
+
+        result shouldBe true
+    }
+}
